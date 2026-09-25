c789a29cc4d21f0d040371839b1800a73ad601db
Hanyia Iftikhar <u3297828@uni.canberra.edu.au>
2026-09-26 00:04:46 +1000

    Complete Week 7 activities

---
+++ Week 7/lab_Week7.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+#patient
+CREATE DATABASE my_database;
+CREATE TABLE patient_record (
+    PersonID: 
+    Name:
+    
+)
+
+#practitioner
+CREATE TABLE practitioner_info(
+    PractitionerID: 
+    Name:
+    
+)
